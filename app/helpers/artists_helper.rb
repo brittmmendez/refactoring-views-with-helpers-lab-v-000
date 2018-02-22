f6ba9c91c749e68c3315ed33456c00b7ed5bbaf9
Brittany Morris <brittmmendez@gmail.com>
2018-02-22 18:06:48 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,5 +1,10 @@
 module ArtistsHelper
 
-
+  def display_artist(artist)
+    if artist
+      <%= link_to artist.name, _path(song.artist) %>
+    else 
+    end
+  end
 
 end
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,5 +1,5 @@
 module SongsHelper
 
-  
+
 
 end
