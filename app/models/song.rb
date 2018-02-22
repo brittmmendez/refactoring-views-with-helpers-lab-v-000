49a4c6739403117ffe283a39766a6cfc3af379e5
Brittany Morris <brittmmendez@gmail.com>
2018-02-22 17:50:49 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,12 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+  def artist_name
+    self.artist.name
+  end
 
+  def artist_name=(name)
+      self.artist=Artist.find_by(name: name)
+
+  end
 
 end
